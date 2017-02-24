35084b21f203da77628c69a4e9df1e045ab7124d
Lucas Colomer <lco@frianbiz.com>
2017-02-24 23:16:02 +0100

    Initial + build

--- sdl2-boilerplate-master/src/main.cpp
+++ sdl2-boilerplate-master/src/main.cpp
@@ -4,19 +4,46 @@
 /*
  * Lesson 0: Test to make sure SDL is setup properly
  */
-int main(int argc, char** argv){
-    std::cout << "HELLO SDL 2";
-    SDL_Init (SDL_INIT_VIDEO);
+int main (int argc, char** argv)
+{
+    SDL_Window* window = NULL;
+    window = SDL_CreateWindow
+    (
+        "Jeu de PIRATE !!!!", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
 
-    SDL_Window *screen;
-    SDL_Renderer *renderer;
+    
+    SDL_Renderer* renderer = NULL;
+    renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
 
-    screen = SDL_CreateWindow("tuto", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
-    renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    
+    SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
 
+    
+    SDL_RenderClear( renderer );
+
+    SDL_Rect r;
+    r.x = 50;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+
+    SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+
+    
+    SDL_RenderFillRect( renderer, &r );
+
+    
     SDL_RenderPresent(renderer);
-    SDL_Delay(10000);
 
+    SDL_Delay( 5000 );
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
